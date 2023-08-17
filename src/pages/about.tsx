d349b98774a843ac262aa427ec21f72dea10eef5
N-ha-1050 <69199126+N-ha-1050@users.noreply.github.com>
2023-08-17 19:44:19 +0900

    ✨ Section の作成

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,3 +1,4 @@
+import { Section } from "@/components/ui/Section"
 import { SetInline } from "@/components/ui/SetInline"
 import { Title } from "@/components/ui/Title"
 import { NextPage } from "next"
@@ -5,8 +6,8 @@ import { NextPage } from "next"
 const About: NextPage = () => (
     <SetInline>
         <Title>About</Title>
-        <h2>introduce</h2>
-        <h2>history</h2>
+        <Section>introduce</Section>
+        <Section>history</Section>
     </SetInline>
 )
 
