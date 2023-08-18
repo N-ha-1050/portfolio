88791e6a7f0f01dd7ab383250db8fa427fea02e8
N-ha-1050 <69199126+N-ha-1050@users.noreply.github.com>
2023-08-18 10:09:56 +0900

    🎨 上下にスペースを追加

--- src/components/ui/Layout.tsx
+++ src/components/ui/Layout.tsx
@@ -5,7 +5,7 @@ const Layout = ({ children }: { children: ReactNode }) => (
     <>
         <Header />
         <div className="bg-blue-50">
-            <main className="mx-auto flex min-h-screen max-w-7xl flex-col justify-center">
+            <main className="mx-auto flex min-h-screen max-w-7xl flex-col justify-center py-16">
                 {children}
             </main>
         </div>
