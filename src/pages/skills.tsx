2862d214c4b2cd43287f3c6c73e27680a519f7b7
N-ha-1050 <69199126+N-ha-1050@users.noreply.github.com>
2023-08-19 09:07:23 +0900

    skills の内容を追加

--- src/pages/skills.tsx
+++ src/pages/skills.tsx
@@ -1,3 +1,4 @@
+import { Section } from "@/components/ui/Section"
 import { SetInline } from "@/components/ui/SetInline"
 import { Title } from "@/components/ui/Title"
 import { NextPage } from "next"
@@ -5,6 +6,76 @@ import { NextPage } from "next"
 const Skills: NextPage = () => (
     <SetInline>
         <Title>Skills</Title>
+        <Section>Python</Section>
+        <p className="m-4">
+            <span>競技プログラミング</span>
+            <br />
+            現在の競プロメイン言語に使用。
+            <br />
+            AtCoder を高校2年のときに始めて、現在の色は緑。
+            <br />
+            <span>Django</span>
+            <br />
+            高校1年のときからいくつかのWebアプリケーションの個人開発に使用。
+            <br />
+            現在は、 Django REST Framework を使った開発がメイン(フロントエンドは
+            Next.js )。
+            <br />
+            認証を実装するライブラリは、 OAuth2 + JWT + Cookie
+            を使うことが多い。
+            <br />
+            <span>データサイエンス・AI</span>
+            <br />
+            高校2年のときに、Django
+            のWebアプリケーションで集めたデータを上手く使えないかと思ったことと、同時期に
+            Word2Vec を触ったことをきっかけに興味を持った。
+            <br />
+            大学で本格的に学びたい(データサイエンス学部在籍)。
+            <br />
+            現在は、Signate や Kaggle のコンペに参加しながら勉強中。メインに
+            LightGBM を使用。
+            <br />
+            <span>その他</span>
+            <br />
+            Python
+            自体は中学3年のときに初めて触り、高校に入ってから本格的に使い始めた。
+            <br />
+            tkinter, Pygame を使ったゲーム開発や Streamlit を使ったWeb開発、
+            Discord Bot を作ったこともある。
+        </p>
+        <Section>TypeScript</Section>
+        <p className="m-4">
+            現在は、 Next.js での開発がメイン。
+            <br />
+            高校2年のときに始めて触り、 Chakra UI を使ってきたが、 Tailwind CSS
+            も勉強中。
+            <br />
+            このサイトの開発言語。
+        </p>
+        <Section>その他の言語</Section>
+        <ul className="m-4">
+            <li>
+                <span>C++, Rust</span> 競技プログラミングに使ったことがある、
+                C++ は初期の頃に使用、 Rust は勉強中(なお、現在のメインは
+                Python)
+            </li>
+            <li>
+                <span>Go</span>{" "}
+                競技プログラミングのために少し勉強し、その後Webのバックエンド開発に使用
+            </li>
+            <li>
+                <span>Ruby, C</span> 高校のときに受けた高大連携の講義で使用
+            </li>
+            <li>
+                <span>Visual Basic</span> vbs
+                は中学の時に初めてプログラミングをした言語、その後 VBA や VB.NET
+                も少し
+            </li>
+            <li>
+                <span>Hot Soup Processor</span>{" "}
+                これも中学の時に使ってた、主にデスクトップアプリ
+            </li>
+        </ul>
     </SetInline>
 )
 
