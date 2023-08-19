f048c9c23f5f82a88c95e0d7105443f7dd81b5d7
N-ha-1050 <69199126+N-ha-1050@users.noreply.github.com>
2023-08-19 09:12:58 +0900

    💄 デザインの変更

--- src/pages/skills.tsx
+++ src/pages/skills.tsx
@@ -8,14 +8,13 @@ const Skills: NextPage = () => (
         <Title>Skills</Title>
         <Section>Python</Section>
         <p className="m-4">
-            <span>競技プログラミング</span>
-            <br />
+            <span className="my-4 block text-xl font-bold">
+                競技プログラミング
+            </span>
             現在の競プロメイン言語に使用。
             <br />
             AtCoder を高校2年のときに始めて、現在の色は緑。
-            <br />
-            <span>Django</span>
-            <br />
+            <span className="my-4 block text-xl font-bold">Django</span>
             高校1年のときからいくつかのWebアプリケーションの個人開発に使用。
             <br />
             現在は、 Django REST Framework を使った開発がメイン(フロントエンドは
@@ -23,9 +22,9 @@ const Skills: NextPage = () => (
             <br />
             認証を実装するライブラリは、 OAuth2 + JWT + Cookie
             を使うことが多い。
-            <br />
-            <span>データサイエンス・AI</span>
-            <br />
+            <span className="my-4 block text-xl font-bold">
+                データサイエンス・AI
+            </span>
             高校2年のときに、Django
             のWebアプリケーションで集めたデータを上手く使えないかと思ったことと、同時期に
             Word2Vec を触ったことをきっかけに興味を持った。
@@ -34,9 +33,7 @@ const Skills: NextPage = () => (
             <br />
             現在は、Signate や Kaggle のコンペに参加しながら勉強中。メインに
             LightGBM を使用。
-            <br />
-            <span>その他</span>
-            <br />
+            <span className="my-4 block text-xl font-bold">その他</span>
             Python
             自体は中学3年のときに初めて触り、高校に入ってから本格的に使い始めた。
             <br />
@@ -55,24 +52,29 @@ const Skills: NextPage = () => (
         <Section>その他の言語</Section>
         <ul className="m-4">
             <li>
-                <span>C++, Rust</span> 競技プログラミングに使ったことがある、
-                C++ は初期の頃に使用、 Rust は勉強中(なお、現在のメインは
-                Python)
+                <span className="my-4 block text-xl font-bold">C++, Rust</span>
+                競技プログラミングに使ったことがある、 C++ は初期の頃に使用、
+                Rust は勉強中(なお、現在のメインは Python )
             </li>
             <li>
-                <span>Go</span>{" "}
+                <span className="my-4 block text-xl font-bold">Go</span>
                 競技プログラミングのために少し勉強し、その後Webのバックエンド開発に使用
             </li>
             <li>
-                <span>Ruby, C</span> 高校のときに受けた高大連携の講義で使用
+                <span className="my-4 block text-xl font-bold">Ruby, C</span>
+                高校のときに受けた高大連携の講義で使用
             </li>
             <li>
-                <span>Visual Basic</span> vbs
-                は中学の時に初めてプログラミングをした言語、その後 VBA や VB.NET
-                も少し
+                <span className="my-4 block text-xl font-bold">
+                    Visual Basic
+                </span>
+                vbs は中学の時に初めてプログラミングをした言語、その後 VBA や
+                VB.NET も少し
             </li>
             <li>
-                <span>Hot Soup Processor</span>{" "}
+                <span className="my-4 block text-xl font-bold">
+                    Hot Soup Processor
+                </span>
                 これも中学の時に使ってた、主にデスクトップアプリ
             </li>
         </ul>
