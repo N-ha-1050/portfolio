0fd930548725552bdb86bf642b9fac7e897fb309
N-ha-1050 <69199126+N-ha-1050@users.noreply.github.com>
2023-08-28 01:46:26 +0900

    背景の変更

--- src/components/ui/Layout.tsx
+++ src/components/ui/Layout.tsx
@@ -4,8 +4,8 @@ import { Header } from "./Header"
 const Layout = ({ children }: { children: ReactNode }) => (
     <>
         <Header />
-        <div className="bg-blue-50">
-            <main className="mx-auto flex min-h-screen max-w-7xl flex-col justify-center py-16">
+        <div className="bg-gradient-to-br from-white from-70% to-blue-400">
+            <main className="mx-auto flex min-h-screen max-w-4xl flex-col justify-center py-16">
                 {children}
             </main>
         </div>
