8bd0f9cc38fb94f6afe4e931297fb57e881b55d2
N-ha-1050 <69199126+N-ha-1050@users.noreply.github.com>
2024-01-11 13:57:10 +0900

    🎨 内容の更新

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,23 +1,15 @@
 import { Content } from "@/components/ui/Content"
 import { Dict } from "@/components/ui/Dict"
-import { List } from "@/components/ui/List"
 import { Part } from "@/components/ui/Part"
 import { Section } from "@/components/ui/Section"
 import { SetInline } from "@/components/ui/SetInline"
 import { Title } from "@/components/ui/Title"
 import { NextPage } from "next"
-// import { Fragment } from "react"
-
-// const history: { date: string; content: string }[] = [
-//     // { date: "2004年7月23日", content: "誕生" },
-//     { date: "2023年3月", content: "桐光学園高等学校 卒業" },
-//     { date: "2023年4月", content: "横浜市立大学 入学" },
-// ]
 
 const About: NextPage = () => (
     <SetInline>
         <Title>About</Title>
-        <Part>introduce</Part>
+        <Part>自己紹介</Part>
         <Content>
             <Section text="2004年7月23日生">
                 <ruby className="mr-4 text-xl font-bold">
@@ -30,7 +22,7 @@ const About: NextPage = () => (
             <br />
             最近は、 Python を使った機械学習と Next.js を使ったWeb開発を勉強中。
         </Content>
-        <Part>history</Part>
+        <Part>経歴</Part>
         <Content>
             <Dict>
                 {/* <>2004年7月23日</>
@@ -41,18 +33,15 @@ const About: NextPage = () => (
                 <>横浜市立大学 入学</>
             </Dict>
         </Content>
-        {/* <Content>
-            <div className="grid w-fit md:grid-cols-2 md:gap-4">
-                {history.map(({ date, content }) => (
-                    <Fragment key={date}>
-                        <p className="justify-self-center font-bold text-blue-800 md:border-b-2 md:border-blue-800 md:text-black">
-                            {date}
-                        </p>
-                        <p className="mb-8 md:mb-0">{content}</p>
-                    </Fragment>
-                ))}
-            </div>
-        </Content> */}
+        <Part>資格</Part>
+        <Content>
+            <Dict>
+                <>2023年7月</>
+                <>TOEIC L&R 645点</>
+                <>2023年12月</>
+                <>普通自動車第一種免許</>
+            </Dict>
+        </Content>
     </SetInline>
 )
 
